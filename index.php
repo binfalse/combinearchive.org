2f4f99b127b7558f625413233c19f8e793dc5c03
Martin Scharm <martin@binfalse.de>
2015-06-11 21:04:56 +0100

    init

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+header("Location: index/");
+
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="refresh" content="0;url=index/">
+        <script type="text/javascript">
+            window.location.href = "index/"
+        </script>
+        <title>COMBINE archive</title>
+    </head>
+    <body>
+        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+        If you are not redirected automatically, follow the <a href='index/'>link to the COMBINE archive website</a>
+    </body>
+</html>
